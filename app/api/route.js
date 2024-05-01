d1af4d1fef00262efd5e2752325dd85b978cfb93
suwizx <suwijak.pak@gmail.com>
2024-05-02 00:13:01 +0700

    final

---
+++ app/api/route.js
@@ -0,0 +1,30 @@
+import { NextResponse , NextRequest } from "next/server";
+import { config } from "dotenv";
+import axios from "axios";
+config()
+
+export async function POST(req){
+    const data = await req.json()
+    const group = data.events[0].source.groupId
+    if(data.events[0].type === "memberJoined"){
+        await axios.post("https://api.line.me/v2/bot/message/push",
+        {
+            "to": group,
+            "messages":[
+                {
+                    "type":"text",
+                    "text":"ยินดีต้อนรับสมาชิกใหม่ค้าบบค้าบ อย่าลืมรายงานตัวในโน้ตด้วยน้า!"
+                }
+            ]
+        },
+        { headers : {
+            Authorization : `Bearer ${process.env.MSG_TOKEN}`
+        } }    
+    )
+    }
+    return NextResponse.json({"message":"success"})
+}
+
+export async function GET(){
+    return NextResponse.json({"message" : "just linebot"})
+}
